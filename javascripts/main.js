284b60040eebb60b43beb0eb583348cc80b198ab
Taylor Shuff <tkylesh@gmail.com>
2016-10-29 10:23:10 -0500

    initliazing branchA with todo list coded with ability to and delete items and to do and complete list laid out.

---
+++ javascripts/main.js
@@ -0,0 +1,17 @@
+"use strict";
+
+$(document).ready(()=>{
+	console.log('jquery is ready');
+	var input = $('input[name=checkListItem]').val();
+    $('#button').click(function(){
+        var toAdd = $('input[name=checkListItem]').val();
+        $('.list').append('<div class="item">'+ toAdd +'</div>');
+    });
+  
+      $(document).on('click', '.item', function() {
+        $(this).remove();
+    });
+
+
+
+});
