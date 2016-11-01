8a061c50a66ae255e643a5e5b7eebdd1c66f424a
Taylor Shuff <tkylesh@gmail.com>
2016-10-31 19:42:41 -0500

    setup firebase

--- javascripts/firebaseMethods.js
+++ javascripts/firebaseMethods.js
@@ -7,7 +7,13 @@ var FbAPI = (function(oldFirebase){
 				method: 'GET',
 				url:`${apiKeys.databaseURL}/items.json`
 			}).then((response)=>{
-				resolve(response);
+				//turn the object response into an array with this code
+				let items = [];
+				Object.keys(response).forEach(function(key){
+					response[key].id = key;
+					items.push(response[key]);
+				});
+				resolve(items);
 			}, (error)=>{
 				reject(error);
 			});
