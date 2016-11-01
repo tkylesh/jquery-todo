9412a127e74e128b63b27ebfe6509315ee950627
Taylor Shuff <tkylesh@gmail.com>
2016-10-31 20:38:25 -0500

    setup firebase

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,20 +6,31 @@ function putTodoInDOM(){
   FbAPI.getTodos(apiKeys).then(function(items){
     console.log("items from FB", items);
     items.forEach(function(item){
-     let newListItem = '<li>';
-      newListItem+='<div class="col-xs-8">';
-      newListItem+='<input class="checkboxStyle" type="checkbox">';
-      newListItem+=`<label class="inputLabel">${item.task}</label>`;
-      newListItem+='<input type="text" class="inputTask">';
-      newListItem+='</div>';
-      newListItem+='<div class="col-xs-4">';
-      newListItem+='<button class="btn btn-default col-xs-6 edit">Edit</button>';
-      newListItem+='<button class="btn btn-danger col-xs-6 delete">Delete</button> ';
-      newListItem+='</div>';
-      newListItem+='</li>';
-      //apend to list
-      $('#incomplete-tasks').append(newListItem);
-
+     if(item.isCompleted === true){
+       let newListItem = '<li>';
+        newListItem+='<div class="col-xs-8">';
+        newListItem+='<input class="checkboxStyle" type="checkbox" checked>';
+        newListItem+=`<label class="inputLabel">${item.task}</label>`;
+        newListItem+='<input type="text" class="inputTask">';
+        newListItem+='</div>';
+        newListItem+='</li>';
+        //apend to list
+        $('#completed-tasks').append(newListItem);
+      }else{
+        let newListItem = '<li>';
+        newListItem+='<div class="col-xs-8">';
+        newListItem+='<input class="checkboxStyle" type="checkbox">';
+        newListItem+=`<label class="inputLabel">${item.task}</label>`;
+        newListItem+='<input type="text" class="inputTask">';
+        newListItem+='</div>';
+        newListItem+='<div class="col-xs-4">';
+        newListItem+='<button class="btn btn-default col-xs-6 edit">Edit</button>';
+        newListItem+='<button class="btn btn-danger col-xs-6 delete">Delete</button> ';
+        newListItem+='</div>';
+        newListItem+='</li>';
+        //apend to list
+        $('#incomplete-tasks').append(newListItem);
+      }
     });
   });
 }
