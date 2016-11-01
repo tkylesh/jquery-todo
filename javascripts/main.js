0a1dd338fdd8e42d2709dd5bee6dadea6b5b5f20
Taylor Shuff <tkylesh@gmail.com>
2016-10-31 20:26:06 -0500

    setup firebase

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,15 +2,35 @@
 
 let apiKeys = {};
 
+function putTodoInDOM(){
+  FbAPI.getTodos(apiKeys).then(function(items){
+    console.log("items from FB", items);
+    items.forEach(function(item){
+     let newListItem = '<li>';
+      newListItem+='<div class="col-xs-8">';
+      newListItem+='<input class="checkboxStyle" type="checkbox">';
+      newListItem+=`<label class="inputLabel">${item.task}</label>`;
+      newListItem+='<input type="text" class="inputTask">';
+      newListItem+='</div>';
+      newListItem+='<div class="col-xs-4">';
+      newListItem+='<button class="btn btn-default col-xs-6 edit">Edit</button>';
+      newListItem+='<button class="btn btn-danger col-xs-6 delete">Delete</button> ';
+      newListItem+='</div>';
+      newListItem+='</li>';
+      //apend to list
+      $('#incomplete-tasks').append(newListItem);
+
+    });
+  });
+}
+
 $(document).ready(()=>{
     console.log('jquery is ready');
     FbAPI.firebaseCredentials().then(function(keys){
       console.log("keys", keys);
       apiKeys = keys;
-        firebase.initializeApp(apiKeys);
-        FbAPI.getTodos(apiKeys).then(function(items){
-          console.log("items from FB", items);
-        });
+      firebase.initializeApp(apiKeys);
+      putTodoInDOM();
     });
 
 
